d55445afb38576f7175ab3cef2fb67f110d8e3fc
Luan Dantas <dantasluan2405@gmail.com>
2022-12-21 05:21:28 -0300

    adicionando o atributo balance e o método deposit

--- OOP/Account.java
+++ OOP/Account.java
@@ -2,10 +2,27 @@
 
 public class Account{
     private String name; //váriavel de instância
+    private double balance;
 
     // o construtor inicializa name com o nome do parâmetro
-    public Account(String name){
+    public Account(String name, double balance){
         this.name = name;
+
+        // valida que o balance é maior que 0.0.
+        if(balance > 0.0) // se o saldo for válido 
+            this.balance = balance; // o atribui á váriavel de instância balance
+    }
+
+    // método que deposita apenas uma quantia válida no saldo
+    public void deposit(double depositAmount){
+        if(depositAmount>0){ // se depositAmount for válido
+            balance += depositAmount; // adiciona ao saldo
+        }
+    }
+
+    // método que retorna o saldo da conta
+    public double getBalance(){
+        return balance;
     }
 
     // método para definir o nome do objeto
--- OOP/Test.java
+++ OOP/Test.java
@@ -1,14 +1,27 @@
 import java.util.Scanner;
- 
+
 public class Test{
     public static void main(String[] args){
         
         // cria dois objetos Account
-        Account account1 = new Account("Jane Doe");
-        Account account2 = new Account("Jhon Doe");
+        Account account1 = new Account("Jane Doe", 50.00);
+        Account account2 = new Account("Jhon Doe", -7.53);
 
-        // exibe o valor inicial de nome para cada Account
-        System.out.printf("Account1 name is %s%n", account1.getName());
-        System.out.printf("account2 name is %s%n", account2.getName());
+        // exibe o saldo inicial de cada Account
+        System.out.printf("%s balance is %.2f %n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance is %.2f %n", account2.getName(), account2.getBalance());
+
+        // cria um Scanner para obter entrada a partir da janela de comando
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a deposit amount for account1: "); //prompt
+        float depositAmount = input.nextFloat(); // entrada do usuário
+        System.out.printf("%nadding %.2f to account1 balance%n%n", depositAmount);
+        account1.deposit(depositAmount); // adiciona o saldo da account1
+
+        //exibe os saldos
+        System.out.printf("%s balance: %.2f %n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: %.2f %n", account2.getName(), account2.getBalance());
+
     }
 }
